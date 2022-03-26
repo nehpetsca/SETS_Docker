164be67f863622f523a4b29a17e19bdc2aeab955
Stephen Hill <nehpets@gmail.com>
2022-03-25 21:16:50 -0700

    Update SETS_build.ps1

--- SETS_build.ps1
+++ SETS_build.ps1
@@ -7,5 +7,5 @@ docker build -t $docker_image_name -f SETS_Docker\SETS.Dockerfile .
 if($?)
 {
     docker rm $docker_container_name 2>$null
-    docker run --name $docker_container_name --volume="$PWD"/STOCD_SETS:/root/LIVE_SETS $docker_image_name
+    docker run --name $docker_container_name  --hostname $docker_container_name --volume="$PWD"/STOCD_SETS:/root/LIVE_SETS $docker_image_name
 }
