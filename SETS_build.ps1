31fc3953f2442169f7bec26e6c5e20cb1bd97143
Stephen Hill <nehpets@gmail.com>
2022-03-25 17:58:14 -0700

    Auto-run with live folder and name/auto-remove legacy container

--- SETS_build.ps1
+++ SETS_build.ps1
@@ -1,2 +1,13 @@
 # To use this as a script, open PowerShell (Admin) and run: Set-ExecutionPolicy RemoteSigned
-docker build -t sets_docker -f SETS_Docker\SETS.Dockerfile .
+$docker_container_name = "SETS"
+$docker_image_name = "sets_docker"
+
+docker build -t $docker_image_name -f SETS_Docker\SETS.Dockerfile .
+
+if($?)
+{
+    docker rm $docker_container_name 2>$null
+    docker run --name $docker_container_name --volume="$PWD"/STOCD_SETS:/root/LIVE_SETS $docker_image_name
+}
+
+
